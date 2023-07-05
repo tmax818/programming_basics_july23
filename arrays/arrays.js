ae3a73a676184ef2c2ff4150476714d1be5496cd
tmax818 <tylermaxwell661@gmail.com>
2023-07-05 10:07:52 -0600

    update

---
+++ arrays/arrays.js
@@ -0,0 +1,17 @@
+
+/**
+ *  file arrays.js
+ * author: Tyler 
+ * date: 
+ */
+
+
+// var student1 = "deven";
+// var student2 = "Tyler";
+
+var students = ['deven', 'tyler', 'zach'];
+
+console.log(students[0])
+
+
+
---
+++ condtionals/conditionals.js
@@ -0,0 +1,13 @@
+
+
+var isLightOn = false;
+
+// conditionals
+
+if (isLightOn) {
+    console.log("It's too bright!!!");
+} else if (3 === 4) {
+    console.log("wrong answer");
+}else {
+    console.log("Dude, turn on the lights!!")
+}
